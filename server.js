06b77e1829651bd3e5d2f6e3fccbef1d06be04bb
Conner McCabe <conner.p.mccabe@gmail.com>
2020-03-11 19:19:52 -0500

    adding logs for connects and disconnects

--- server.js
+++ server.js
@@ -2,15 +2,23 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+users = [];
+connections = [];
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg)
-  })
-});
+io.on('connection', function(socket) {
+  //connections
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  //disconnect
+  connections.splice(connections.indexOf(socket), 1);
+  console.log('Disconnected: %s sockets connected', connections.length)
+})
+
 
 http.listen(3000, function() {
   console.log('Listening on port 3000');
