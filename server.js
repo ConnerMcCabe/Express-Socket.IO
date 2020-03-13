ea817923e5ecc8258482faa2f786ba8110f586ba
Conner McCabe <conner.p.mccabe@gmail.com>
2020-03-13 14:35:36 -0500

    new take

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ io.on('connection', function(socket) {
 })
 
 
-http.listen(3000, function() {
-  console.log('Listening on port 3000');
+http.listen(5000, function() {
+  console.log('Listening on port 5000');
 })
