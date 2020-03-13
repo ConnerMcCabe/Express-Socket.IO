a1c197d8a337f4b6ae74159bf02a71ce514c580d
Conner McCabe <conner.p.mccabe@gmail.com>
2020-03-13 15:25:52 -0500

    simplified socket.io

--- server.js
+++ server.js
@@ -2,31 +2,10 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-users = [];
-connections = [];
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket) {
-  //connections
-  connections.push(socket);
-  console.log('Connected: %s sockets connected', connections.length);
-
-  //disconnect
-  socket.on('disconnect', function(data) {
-    connections.splice(connections.indexOf(socket), 1);
-    console.log('Disconnected: %s sockets connected', connections.length)
-
-  //send messages
-  socket.on('send message', function(data){
-    console.log(data)
-    io.sockets.emit('new message', {msg: data});
-  })
-  })
+io.on('connection', socket => {
+  console.log('new User')
+  socket.emit('chat-message', "holla");
 })
-
 
 http.listen(5000, function() {
   console.log('Listening on port 5000');
